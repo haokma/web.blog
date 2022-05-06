31a2a5463d0a00068c8bc69f450497004b6c5a3e
hao2001vt <nguyenchihao2001vt@gmail.com>
2022-05-07 01:43:21 +0700

    Add tailwind

--- components/commons/Header/header.tsx
+++ components/commons/Header/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div>
-      <h1>Header Component</h1>
+      <h1 className="text-5xl text-primary">Header Component</h1>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4AD295',
+        secondary: '#0b0b0c',
+        heading: '#242428',
+        paragraph: '#666666',
+      },
+    },
+  },
+  plugins: [],
+};
